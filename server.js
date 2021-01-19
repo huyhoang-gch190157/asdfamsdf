40d674f168069ddb731daf300e04396c2b403411
huyhoang-gch190157 <77625101+huyhoang-gch190157@users.noreply.github.com>
2021-01-19 16:36:43 +0700

    make it better

---
+++ server.js
@@ -0,0 +1,44 @@
+var http = require('http'); // 1 - Import Node.js core module
+var url = require('url');
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });  
+        
+        // set response content    
+        res.write('<html><body><h1>This is home Page.</h1>');
+        res.write('<a href="http://localhost:5000/student"> Student</a><br>');
+        res.write('<a href="http://localhost:5000/admin">Admin</a></body></html><br>');
+        res.end();
+    
+    }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><h1>This is student Page.</h1></body>');
+        res.write('<a href="http://localhost:5000">Home</a><br>');
+        res.write('<a href="http://localhost:5000/admin">Admin</a></html><br>');
+        res.end();
+    
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><h1>This is admin Page.</h1></body>');
+        res.write('<a href="http://localhost:5000/student">Student</a><br>');
+        res.write('<a href="http://localhost:5000">Home</a></html><br>');
+        res.end();
+    
+    }
+    else
+        res.end('Invalid Request!');
+    //handle incomming requests here..
+
+});
+
+var PORT = process.env.PORT || 5000
+server.listen(PORT); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
